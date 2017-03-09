46635edea436664628b238ae8c647eec07467320
marinams <maksim.s.marina@gmail.com>
2017-03-09 20:50:03 +0300

    add Filter data email list

--- app/app.js
+++ app/app.js
@@ -129,6 +129,7 @@ app.component('contact', contactComponent);
 //Закоментировал, т.к. через експорт не получилось сделать.
 //app.service('EmailService', EmailService);
 
+/****** SERVICE *******/
 app.service('EmailService', function ($http) {
     this.getAll = () => {
         return $http.get('./data/emails.json');
@@ -141,6 +142,7 @@ app.service('ContactService', function ($http) {
     }
 });
 
+/****** FILTERS *******/
 app.filter('emailByTypeFilter', function () {
     return function (items, type) {
         var filtered = [];
@@ -171,4 +173,22 @@ app.filter('counterEmailFilter', function () {
         }
         return count;
     };
+});
+
+app.filter('dateAgo', function() {
+    return function dateAgo(date_p) {
+        var now_date = new Date();
+        var diff = now_date.getTime() - Date.parse(date_p);
+        if ( diff <= 120000){ // 2min
+            return 'Just now';
+        }
+        if ( diff <= 60*60000){ // min
+            return Math.floor(diff / 60000) + ' minutes ago';
+        }
+        if ( diff <= 24*60*60000){
+            return Math.floor(diff / 3600000) + ' hours ago';
+        }
+        //days
+        return Math.floor(diff / 24 / 3600000) + ' days ago';
+    }
 });
